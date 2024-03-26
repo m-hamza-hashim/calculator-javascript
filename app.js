accb3ce6d649886b62daa6384caebcbc3849c8dc
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-03-26 14:48:27 +0500

    completed

--- app.js
+++ app.js
@@ -8,51 +8,122 @@ document.addEventListener("keydown", function(event) {
     event.preventDefault();
 });
 
-function digit(btn) {
-    inp1.value += btn; 
+function digit (btn) {
+    if (inp1.value.length < 9) {
+        inp1.value += btn; 
+    }
 }
+
+var equClicked = false;
 
 var classNames = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-for (i = 0; i <= 9; i++) {
-    // Get the button element
-    var button = document.getElementsByClassName(classNames[i])[0];
-    // Add event listener to the button
-    button.addEventListener("click", function() {
-        digit(i);
-    });
+for (var i = 0; i <= 9; i++) {
+    (function(index) {
+        var button = document.getElementsByClassName(classNames[index])[0];
+        button.addEventListener("click", function() {
+            if (!equClicked) {
+                digit(`${index}`);
+            }
+        });
+    })(i);
 }
 
+document.getElementsByClassName("dec")[0].addEventListener("click", function () {
+    if (inp1.value.indexOf(".") === -1) {
+        if (!equClicked) {
+            digit(".");
+        }
+    }
+});
 
-// document.getElementsByClassName("one")[0].addEventListener("click", function() {
-//     digit(1);
-// });
-// document.getElementsByClassName("two")[0].addEventListener("click", function() {
-//     digit(2);
-// });
-// document.getElementsByClassName("three")[0].addEventListener("click", function() {
-//     digit(3);
-// });
-// document.getElementsByClassName("four")[0].addEventListener("click", function() {
-//     digit(4);
-// });
-// document.getElementsByClassName("five")[0].addEventListener("click", function() {
-//     digit(5);
-// });
-// document.getElementsByClassName("six")[0].addEventListener("click", function() {
-//     digit(6);
-// });
-// document.getElementsByClassName("seven")[0].addEventListener("click", function() {
-//     digit(7);
-// });
-// document.getElementsByClassName("eight")[0].addEventListener("click", function() {
-//     digit(8);
-// });
-// document.getElementsByClassName("nine")[0].addEventListener("click", function() {
-//     digit(9);
-// });
-// document.getElementsByClassName("zero")[0].addEventListener("click", function() {
-//     digit(0);
-// });
+var nos = [];
+var operator;
+
+var operations = ["add", "sub", "mul", "div", "mod"];
+
+function operationSetup(fun) {
+    nos.push(inp1.value);
+    operator = fun;
+    inp1.value = "";
+}
+
+for (var i = 0; i < operations.length; i++) {
+    (function(index) {
+        document.getElementsByClassName(operations[index])[0].addEventListener("click", function () {
+            if (!equClicked) {
+                operationSetup(operations[index]);
+            }
+        });
+    })(i);
+}
+
+var sum = 0, mul = 1;
+var sub, div, mod;
+
+var inp2 = document.getElementsByClassName("inp2")[0];
+
+document.getElementsByClassName("equ")[0].addEventListener("click", function () {
+    nos.push(inp1.value);
+    switch (operator) {
+        case "add":
+            for (var i = 0; i < nos.length; i++) {
+                sum += Number(nos[i]);
+            }
+            inp2.value = sum;
+            sum = 0;
+            break;
+        case "sub":
+            for (var i = 1; i < nos.length; i++) {
+                sum += Number(nos[i]);
+            }
+            sub = Number(nos[0]) - sum;
+            inp2.value = sub;
+            sub = 0;
+            break;
+        case "mul":
+            for (var i = 0; i < nos.length; i++) {
+                mul *= Number(nos[i]);
+            }
+            inp2.value = mul;
+            mul = 0;
+            break;
+        case "div":
+            div = Number(nos[0]);
+            for (var i = 1; i < nos.length; i++) {
+                div /= Number(nos[i]);
+            }
+            inp2.value = div;
+            div = 0;
+            break;
+        case "mod":
+            mod = Number(nos[0]);
+            for (var i = 0; i < nos.length; i++) {
+                mod %= Number(nos[i]);
+            }
+            inp2.value = mod;
+            mod = 0;
+            break;
+    }
+    nos = [];
+    operator = "";
+    equClicked = true;
+    }
+);
+
+document.getElementsByClassName("c")[0].addEventListener("click", function () {
+    inp1.value = "";
+    equClicked = false;
+});
+
+document.getElementsByClassName("del")[0].addEventListener("click", function () {
+    if (!equClicked) {
+        inp1.value = inp1.value.slice(0, -1);
+    }
+});
+
+
+
+
 
 
